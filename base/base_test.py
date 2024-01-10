335f2ffc243354aabc896572eda497d0e2f7690d
Tatiana Yurchenko <taf88156@mail.ru>
2024-01-10 15:30:54 +0300

    feat: add authorization section

--- base/base_test.py
+++ base/base_test.py
@@ -2,12 +2,13 @@
 #  импортирум все пэйджи
 # и используем аннотацию типов python
 import pytest
-
+from config.data import Data
 from pages.personal_page import PersonalPage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 class BaseTest:
 
+    data: Data
     login_page: LoginPage
     dashboard_page: DashboardPage
     personal_page: PersonalPage
@@ -19,6 +20,7 @@ class BaseTest:
     @pytest.fixture(autouse=True)
     def setup(self, request, driver):
         request.cls.driver = driver
+        request.cls.data = Data()
         request.cls.login_page = LoginPage(driver)
         request.cls.dashboard_page = DashboardPage(driver)
         request.cls.personal_page = PersonalPage(driver)
--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -1,3 +1,5 @@
+import random
+
 from base.base_test import BaseTest
 #  дает мультистраничный доступ
 
@@ -6,12 +8,12 @@ class ProfileFeatureTests(BaseTest):
 
     def test_change_profile_name(self):
         self.login_page.open()
-        self.login_page.enter_login()
-        self.login_page.enter_password()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
         self.dashboard_page.is_opened()
         self.dashboard_page.click_my_info_link()
         self.personal_page.is_opened()
-        self.personal_page.change_name()
+        self.personal_page.change_name(f"Test{random.randint(1,100)}")
         self.personal_page.save_changes()
         self.personal_page.is_changes_saved()
 
